fa187b49bc58bfe7ae67e69ed1dba847822b2aa7
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-05-13 11:55:36 -0600

    set prefix classnames for production

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import {StylesProvider} from '@material-ui/core/styles';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-const App = () => <div>
-  <StylesProvider>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route  path="/pricing" component={Pricing} />
-      </Switch>
-    </BrowserRouter>
-  </StylesProvider>
-</div>
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
+const App = () => (
+  <div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/pricing" component={Pricing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
+  </div>
+);
 
 
 export default App;
