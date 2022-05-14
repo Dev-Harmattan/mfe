fe4271e587c1a91e63d6d69aa5ae59fd25248f55
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-05-14 08:26:19 -0600

    resolving development performance issue on publicpath

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {createMemoryHistory, createBrowserHistory} from 'history';
+
+const mount = (devRoot, {onNavigate, defaultHistory}) => {
+  const history = defaultHistory || createMemoryHistory();
+  if(onNavigate){
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, devRoot);
+
+  return {
+    onParentNavigate: ({pathname: nextPathname}) => {
+      const {pathname} = history.location
+      if(nextPathname !== pathname){
+        history.push(nextPathname);
+      }
+    }
+  }
+}
+
+//When running the remote on Dev and in Isolation.
+//We should render the element immediately.
+if(process.env.NODE_ENV === 'development'){
+  const devRoot = document.getElementById('_auth_dev_root');
+  if(devRoot){
+    mount(devRoot, {defaultHistory: createBrowserHistory()});
+  }
+}
+
+//When running by container (Host).
+//We should export the mount function.
+export { mount }
